41d73b17790785e7549eddce8f16bca3eb925406
rus733 <9282716617@mail.ru>
2021-12-22 16:46:24 +0300

    добавили webpack.config  с указанием path и main.js

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-(()=>{var o={509:o=>{o.exports=()=>{console.log("two")}}},r={};function t(e){var s=r[e];if(void 0!==s)return s.exports;var n=r[e]={exports:{}};return o[e](n,n.exports,t),n.exports}(()=>{"use strict";const o=t(509);console.log("one"),o()})()})();
+(()=>{"use strict";console.log("one"),console.log("one с импортом и дефолт"),console.log("two")})();

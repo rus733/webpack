0b41ccbd585e3bbf3609df810892147be3b4cd53
rus733 <9282716617@mail.ru>
2021-12-22 16:51:02 +0300

    добавили папку src в контекст  с помощью path.resolve()

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path'); // библиотека из Node,формирует абс путь
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'), //папку src внесли в контекст и убрали
+  entry: './index.js', // убрали из этого пути
+  output: {
+    // в обьект передаем
+    filename: 'main.js', // имя исходящего файла
+    path: path.resolve(__dirname, 'dist'), // абсолютный путь куда он помещается
+  },
+};
